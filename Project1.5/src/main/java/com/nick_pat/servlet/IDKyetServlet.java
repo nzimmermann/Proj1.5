56c7dafcd94cb04b6ffed889c750bde2c6c85216
Patrick Gonzalez <pgonzalez017@gmail.com>
2021-04-02 10:34:55 -0400

    Created a class for servlet and the threads we will use along with the web xml AKA deployment descriptor

---
+++ Project1.5/src/main/java/com/nick_pat/servlet/IDKyetServlet.java
@@ -0,0 +1,33 @@
+package com.nick_pat.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// TODO: Configure this servlet properly and rename it afterwards
+public class IDKyetServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+}
---
+++ Project1.5/src/main/java/com/nick_pat/util/ProjectThreads.java
@@ -0,0 +1,4 @@
+package com.nick_pat.util;
+
+public class ProjectThreads {
+}
